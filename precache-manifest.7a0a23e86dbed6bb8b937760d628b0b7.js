29ce18fb8349b6388c56f5b229b398202b8594b9
Sharaf10 <sharafkiyasdeen123@gmail.com>
2022-10-28 09:53:16 +0530

    Updates

---
+++ precache-manifest.7a0a23e86dbed6bb8b937760d628b0b7.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = [
+  {
+    "revision": "1240fd375075f75e81d4",
+    "url": "/sharaf_portfolio/static/css/main.d7103857.chunk.css"
+  },
+  {
+    "revision": "1240fd375075f75e81d4",
+    "url": "/sharaf_portfolio/static/js/main.1240fd37.chunk.js"
+  },
+  {
+    "revision": "5d66ad4a2cc4efba40a3",
+    "url": "/sharaf_portfolio/static/js/1.5d66ad4a.chunk.js"
+  },
+  {
+    "revision": "273b75ccef1373069ddb",
+    "url": "/sharaf_portfolio/static/js/runtime~main.273b75cc.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/sharaf_portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/sharaf_portfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/sharaf_portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/sharaf_portfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/sharaf_portfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/sharaf_portfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/sharaf_portfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/sharaf_portfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "1c5c79ca322d74bf7f6cf924aa95638d",
+    "url": "/sharaf_portfolio/static/media/abdul.1c5c79ca.jpg"
+  },
+  {
+    "revision": "a1051a228140c97862ba9a7f321c92b6",
+    "url": "/sharaf_portfolio/static/media/buld.a1051a22.jpg"
+  },
+  {
+    "revision": "1c78cfca5f2bb98cc71d5dfc3832c436",
+    "url": "/sharaf_portfolio/static/media/code.1c78cfca.jpeg"
+  },
+  {
+    "revision": "102de84c37df87b447e7515e90bebda2",
+    "url": "/sharaf_portfolio/static/media/cosera.102de84c.jpeg"
+  },
+  {
+    "revision": "404ae7bc061a03e399daa61344c78566",
+    "url": "/sharaf_portfolio/static/media/cwoc.404ae7bc.png"
+  },
+  {
+    "revision": "82473004d117a65c8ed30396c6299763",
+    "url": "/sharaf_portfolio/static/media/efi.82473004.png"
+  },
+  {
+    "revision": "62a5c425bfd0c5eab9f4a08ab4052026",
+    "url": "/sharaf_portfolio/static/media/esoft.62a5c425.png"
+  },
+  {
+    "revision": "54f6721948491acb1997fd43496cd7df",
+    "url": "/sharaf_portfolio/static/media/flutter.54f67219.png"
+  },
+  {
+    "revision": "6364dbb3a5b59c28566eac7e6aaacd04",
+    "url": "/sharaf_portfolio/static/media/gdg.6364dbb3.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/sharaf_portfolio/static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/sharaf_portfolio/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "7b790ddae0e6d6491b271a633fb45526",
+    "url": "/sharaf_portfolio/static/media/hackerrankjava.7b790dda.png"
+  },
+  {
+    "revision": "d93e4c7218360ba2de0adb4c0f14679b",
+    "url": "/sharaf_portfolio/static/media/hackerrankjava1.d93e4c72.png"
+  },
+  {
+    "revision": "08e786f273b5bd93f71b6c00ce8e5f89",
+    "url": "/sharaf_portfolio/static/media/ibm.08e786f2.png"
+  },
+  {
+    "revision": "f9100534b2ab8992537250489be64273",
+    "url": "/sharaf_portfolio/static/media/iet.f9100534.png"
+  },
+  {
+    "revision": "863ea3db49c7fd35b5683b4d86b38261",
+    "url": "/sharaf_portfolio/static/media/linkedin.863ea3db.jpg"
+  },
+  {
+    "revision": "847873a85df8045d1d51cc6cf9cc97d7",
+    "url": "/sharaf_portfolio/static/media/localguide.847873a8.gif"
+  },
+  {
+    "revision": "b7f903d747b0409347c9ee67745dbfa4",
+    "url": "/sharaf_portfolio/static/media/localguide.b7f903d7.png"
+  },
+  {
+    "revision": "d324ee17433678f2c32e57198b98c6db",
+    "url": "/sharaf_portfolio/static/media/mongo.d324ee17.png"
+  },
+  {
+    "revision": "7183465c0d9040eb4f05df22d153eee5",
+    "url": "/sharaf_portfolio/static/media/myself.7183465c.png"
+  },
+  {
+    "revision": "16f34a725e7f648b5dec39417ac4722d",
+    "url": "/sharaf_portfolio/static/media/myself1.16f34a72.png"
+  },
+  {
+    "revision": "a20ed0c9ce1aa32ff49bd680a7fc039d",
+    "url": "/sharaf_portfolio/static/media/photoshop.a20ed0c9.jpg"
+  },
+  {
+    "revision": "22f11067f2d745aaceb51e485720635d",
+    "url": "/sharaf_portfolio/static/media/postman.22f11067.jpg"
+  },
+  {
+    "revision": "c108f55777ceaa6690606a207b737d0a",
+    "url": "/sharaf_portfolio/static/media/postman.c108f557.png"
+  },
+  {
+    "revision": "9a9f2d356934ef87d3228877662957ab",
+    "url": "/sharaf_portfolio/static/media/sharaf.9a9f2d35.jpeg"
+  },
+  {
+    "revision": "8404c26a960018397bd73b4e65f50f59",
+    "url": "/sharaf_portfolio/static/media/site-logo.8404c26a.png"
+  },
+  {
+    "revision": "a4ae2206f581a4f7c547f386fdc282e8",
+    "url": "/sharaf_portfolio/static/media/uddemypy.a4ae2206.jpg"
+  },
+  {
+    "revision": "130aaecd9bc5fa0a7a37f700ec5c8c71",
+    "url": "/sharaf_portfolio/static/media/zahira.130aaecd.jpeg"
+  },
+  {
+    "revision": "f327680f2f5e0b4b0d69c69c4429d743",
+    "url": "/sharaf_portfolio/static/media/zcc.f327680f.jpg"
+  },
+  {
+    "revision": "352d3672c558971741d3ad5ae1d89907",
+    "url": "/sharaf_portfolio/static/media/zcc.png.352d3672.crdownload"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/sharaf_portfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/sharaf_portfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/sharaf_portfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/sharaf_portfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/sharaf_portfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/sharaf_portfolio/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/sharaf_portfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/sharaf_portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/sharaf_portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/sharaf_portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/sharaf_portfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/sharaf_portfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/sharaf_portfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/sharaf_portfolio/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/sharaf_portfolio/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "6160ea26d30c9746b94f972b2ec45ac1",
+    "url": "/sharaf_portfolio/index.html"
+  }
+];
